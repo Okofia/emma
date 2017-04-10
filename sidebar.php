2c36bc7c4abf0ac89f262a3e0ab34f1c2e991463
Okofia <theokofia@yahoo.com>
2017-04-10 09:35:19 +0100

    ok

---
+++ sidebar.php
@@ -0,0 +1,395 @@
+ <aside class="left-side sidebar-offcanvas">
+        <!-- sidebar: style can be found in sidebar-->
+        <section class="sidebar">
+            <div id="menu" role="navigation">
+                <div class="nav_profile">
+                    <div class="media profile-left">
+                        <a class="pull-left profile-thumb" href="#">
+                            <img src="img/authors/avatar1.jpg" class="img-circle" alt="User Image"></a>
+                        <div class="content-profile">
+                            <h4 class="media-heading">Addison</h4>
+                            <ul class="icon-list">
+                                <li>
+                                    <a href="user_profile.php">
+                                        <i class="fa fa-fw ti-user"></i>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="edit_user.php">
+                                        <i class="fa fa-fw ti-settings"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="login.html">
+                                        <i class="fa fa-fw ti-shift-right"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <ul class="navigation">
+
+                    <li class="active" id="active">
+                        <a href="index.php">
+                            <i class="menu-icon ti-desktop"></i>
+                            <span class="mm-text ">Dashboard</span>
+                        </a>
+                    </li>
+                  <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-user"></i>
+                            <span>My Account</span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="edit_user.php">
+                                    <i class="fa fa-fw ti-check-box"></i> Edit Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="users.php">
+                                    <i class="fa fa-fw ti-receipt"></i> Site Users
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-gallery"></i>
+                            <span>Letter of Happiness</span>
+                        </a>
+                    </li>
+
+
+                    <li class="menu-dropdown">
+                        <a href="javascript:void(0)">
+                            <i class="menu-icon ti-check-box"></i>
+                            <span>Forms</span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="javascript:void(0)">
+                                    <i class="fa fa-fw ti-receipt"></i> Features
+                                    <span class="fa arrow"></span>
+                                </a>
+                                <ul class="sub-menu p-l-40">
+                                    <li>
+                                        <a href="form_elements.html">
+                                            <i class="fa fa-fw ti-cup"></i> Form Elements
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="form_validations.html">
+                                            <i class="fa fa-fw ti-alert"></i> Form Validations
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="form_layouts.html">
+                                            <i class="fa fa-fw ti-layout-width-default"></i> Form Layouts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="complex_forms.html">
+                                            <i class="fa fa-fw ti-layout-cta-left"></i> Complex Forms
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="complex_forms2.html">
+                                            <i class="fa fa-fw ti-layout-cta-center"></i> Complex Forms 2
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="realtime_form.html">
+                                            <i class="fa fa-fw ti-write"></i> Realtime Forms
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="radio_checkboxes.html">
+                                            <i class="fa fa-fw ti-check-box"></i> Radio and Checkbox
+                                        </a>
+                                    </li>
+
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">
+                                    <i class="fa fa-fw ti-clipboard"></i> Components
+                                    <span class="fa arrow"></span>
+                                </a>
+                                <ul class="sub-menu p-l-40">
+                                    <li>
+                                        <a href="form_editors.html">
+                                            <i class="fa fa-fw ti-pencil"></i> Form Editors
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="form_wizards.html">
+                                            <i class="fa fa-fw ti-settings"></i> Form Wizards
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="dropdowns.html">
+                                            <i class="fa fa-fw ti-widget-alt"></i> Drop Downs
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="datepicker.html">
+                                            <i class="fa fa-fw ti-calendar"></i> Date pickers
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="advanceddate_pickers.html">
+                                            <i class="fa fa-fw ti-notepad"></i> Advanced Date pickers
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="x-editable.html">
+                                            <i class="fa fa-fw ti-slice"></i> X-editable
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-desktop"></i>
+                            <span>
+                                    UI Features
+                                </span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="general_components.html">
+                                    <i class="fa fa-fw ti-plug"></i> General Components
+                                </a>
+                            </li>
+                            <li>
+                                <a href="buttons.html">
+                                    <i class="fa fa-fw ti-layout-placeholder"></i> Buttons
+                                </a>
+                            </li>
+                            <li>
+                                <a href="tabs_accordions.html">
+                                    <i class="fa fa-fw ti-layers"></i> Tabs &amp; Accordions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="fonts.html">
+                                    <i class="fa fa-fw ti-ink-pen"></i> Font Icons
+                                </a>
+                            </li>
+                            <li>
+                                <a href="advanced_modals.html">
+                                    <i class="fa fa-fw ti-brush-alt"></i> Advanced Modals
+                                </a>
+                            </li>
+                            <li>
+                                <a href="timeline.html">
+                                    <i class="fa fa-fw ti-time"></i> Timeline
+                                </a>
+                            </li>
+                            <li>
+                                <a href="notifications.html">
+                                    <i class="fa fa-fw ti-flag-alt"></i> Notifications
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-briefcase"></i>
+                            <span>
+                                    UI Components
+                                </span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="pickers.html">
+                                    <i class="fa fa-fw ti-brush"></i> Pickers
+                                </a>
+                            </li>
+                            <li>
+                                <a href="grid_layout.html">
+                                    <i class="fa fa-fw ti-layout-grid2"></i> Grid Layout
+                                </a>
+                            </li>
+                            <li>
+                                <a href="tags_input.html">
+                                    <i class="fa fa-fw ti-tag"></i> Tags Input
+                                </a>
+                            </li>
+                            <li>
+                                <a href="nestable_list.html">
+                                    <i class="fa fa-fw ti-view-list"></i> Nestable List
+                                </a>
+                            </li>
+                            <li>
+                                <a href="sweet_alert.html">
+                                    <i class="fa fa-fw ti-bell"></i> Sweet Alert
+                                </a>
+                            </li>
+                            <li>
+                                <a href="toastr_notifications.html">
+                                    <i class="fa fa-fw ti-tablet"></i> Toastr Notifications
+                                </a>
+                            </li>
+                            <li>
+                                <a href="draggable_portlets.html">
+                                    <i class="fa fa-fw ti-control-shuffle"></i> Draggable Portlets
+                                </a>
+                            </li>
+                            <li>
+                                <a href="transitions.html"> <i class="fa fa-fw ti-star"></i> Transitions </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-layout-grid4"></i>
+                            <span>DataTables</span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="simple_tables.html">
+                                    <i class="fa fa-fw ti-layout"></i> Simple tables
+                                </a>
+                            </li>
+                            <li>
+                                <a href="datatables.html">
+                                    <i class="fa fa-fw ti-server"></i> Data Tables
+                                </a>
+                            </li>
+                            <li>
+                                <a href="advanced_datatables.html">
+                                    <i class="fa fa-fw ti-layout-grid3"></i> Advanced Tables
+                                </a>
+                            </li>
+                            <li>
+                                <a href="responsive_datatables.html">
+                                    <i class="fa fa-fw ti-layout-accordion-merged"></i> Responsive DataTables
+                                </a>
+                            </li>
+                            <li>
+                                <a href="bootstrap_tables.html">
+                                    <i class="fa fa-fw ti-layout-grid2"></i> Bootstrap Tables
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+
+
+
+                    <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-gallery"></i>
+                            <span>Gallery</span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="masonry_gallery.html">
+                                    <i class="fa fa-fw ti-gallery"></i> Masonry Gallery
+                                </a>
+                            </li>
+                            <li>
+                                <a href="multiplefile_upload.html">
+                                    <i class="fa fa-fw ti-upload"></i> Multiple File Upload
+                                </a>
+                            </li>
+                            <li>
+                                <a href="dropify.html">
+                                    <i class="fa fa-fw ti-dropbox"></i> Dropify
+                                </a>
+                            </li>
+                            <li>
+                                <a href="image_hover.html">
+                                    <i class="fa fa-fw ti-image"></i> Image Hover
+                                </a>
+                            </li>
+                            <li>
+                                <a href="image_filter.html">
+                                    <i class="fa fa-fw ti-filter"></i> Image Filter
+                                </a>
+                            </li>
+                            <li>
+                                <a href="image_magnifier.html">
+                                    <i class="fa fa-fw ti-zoom-in"></i> Image Magnifier
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+
+                    <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-location-pin"></i>
+                            <span>Maps</span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="google_maps.html">
+                                    <i class="fa fa-fw ti-world"></i> Google Maps
+                                </a>
+                            </li>
+                            <li>
+                                <a href="vector_maps.html">
+                                    <i class="fa fa-fw ti-map"></i> Vector Maps
+                                </a>
+                            </li>
+                            <li>
+                                <a href="advanced_maps.html">
+                                    <i class="fa fa-fw ti-map-alt"></i> Advanced Maps
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="menu-dropdown">
+                        <a href="#">
+                            <i class="menu-icon ti-layout-grid3"></i>
+                            <span>Layouts</span>
+                            <span class="fa arrow"></span>
+                        </a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="menubarfold.html">
+                                    <i class="fa fa-fw ti-layout-menu-v"></i> Menubar Fold
+                                </a>
+                            </li>
+                            <li>
+                                <a href="boxed_movable_header.html">
+                                    <i class="fa fa-fw ti-layout-grid2"></i> Boxed &amp; Movable Header
+                                </a>
+                            </li>
+                            <li>
+                                <a href="layout_movable_header.html">
+                                    <i class="fa fa-fw ti-view-list-alt"></i> Movable Header
+                                </a>
+                            </li>
+                            <li>
+                                <a href="layout_boxed_fixed_header.html">
+                                    <i class="fa fa-fw ti-view-list"></i> Boxed &amp; Fixed Header
+                                </a>
+                            </li>
+                            <li>
+                                <a href="layout_fixed.html">
+                                    <i class="fa fa-fw ti-layout-column2"></i> Fixed Header &amp; Menu
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <!-- / .navigation --> </div>
+            <!-- menu --> </section>
+        <!-- /.sidebar --> </aside>
